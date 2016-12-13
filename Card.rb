68502f8ed672e17deaa1210072013a6968cf0966
David Allen <dma@gnu.org>
2016-12-13 09:44:31 -0500

    Commented secondary classes

--- Card.rb
+++ Card.rb
@@ -1,19 +1,26 @@
 class Card
+	#Has to be initialized with variables
 	def initialize(number, suit, face)
+		#card value
 		@number = number
+		#card suit
 		@suit = suit
+		#card superficial name
 		@face = face		
 	end
 
 	def disp
+		#States the card in a pretty way
 		return "#{@face} of #{@suit} (#{number})"
 	end
 
 	def number
+		#gets the value
 		return @number.to_i
 	end
 
 	def face
+		#states the card's face
 		return @face
 	end
 end
--- Hand.rb
+++ Hand.rb
@@ -1,32 +1,41 @@
 class Hand
+	#Array to hold the card objects
 	@hand_contents = []
 
+	#no innate variables
 	def initialize
 	end
 
+	#gets the total of the card values, lot's of DRY with this
 	def sum
-		
+		#sets sum at 0
 		sum = 0
 		
+		#adds each card to the sum
 		@hand_contents.each do |card|
 			sum += card.number
 		end
-		
+
+		#return the sum		
 		return sum
 	end
 
+	#adds a card to the array via push
 	def add(card)
 		@hand_contents.push(card)
 	end
 
+	#returns the actual array
 	def array
 		return @hand_contents
 	end
 
+	#clears the hand
 	def clear
 		@hand_contents = []
 	end
 
+	#prints all of the cards in the hand using the pretty card output
 	def disp
 		@hand_contents.each do |card|
 			puts card.disp
